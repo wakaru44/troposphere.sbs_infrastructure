91668a997f00b1036646a32781b5a14576f1f4cb
wakaru <wakaru44@gmail.com>
2016-05-01 14:13:06 +0200

    removing dns from the main file for now

--- personal_stack.py
+++ personal_stack.py
@@ -42,35 +42,12 @@ echo "this should be a script or sumzin"
 # Add instance to template
 template.add_resource(instance)
 
-# Add the machine to a nicer dns name
-hostedzone = template.add_parameter(Parameter(
-    "HostedZone",
-    Description="The DNS name of an existing Amazon Route 53 hosted zone",
-    Type="String",
-))
-
-
-personalDNSRecord = template.add_resource(RecordSetType(
-    "personalDNSRecord",
-    HostedZoneName=Join("", [Ref(hostedzone), "."]),
-    Comment="DNS name for the personal instance.",
-    Name=Join("", [Ref(instance), ".", Ref("AWS::Region"), ".",
-              Ref(hostedzone), "."]),
-    Type="A",
-    TTL="900",
-    ResourceRecords=[GetAtt("PersonalInstance", "PublicIp")],
-))
-
-
 # Add output to template
 template.add_output(Output(
     "InstanceAccess",
     Description="Command to use to SSH to instance",
     Value=Join("", ["ssh -i ", Ref(keypair), " ubuntu@", GetAtt(instance, "PublicDnsName")])
 ))
-
-# Add DNS to the output too
-template.add_output(Output("DomainName", Value=Ref(personalDNSRecord)))
 
 
 # Print out CloudFormation template in JSON
